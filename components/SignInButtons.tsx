22d917d15fa569aa59c34180bc1a9d0323a06138
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-07 19:23:48 +0530

    added class name from global css

--- components/SignInButtons.tsx
+++ components/SignInButtons.tsx
@@ -5,13 +5,13 @@ export default function SignInButtons() {
     <>
       <h1 className="text-center mt-8">Sign In</h1>
       <div className="mt-4 p-4 flex flex-col items-center justify-center gap-4">
-        <button className="flex items-center border p-4 rounded-md gap-4 hover:bg-slate-100/25 transition">
+        <button className="singup-btns">
           <span>
             <Image src={'/github-logo.svg'} width={30} height={30} alt={'Github Logo'}/>
           </span>
           Sign In with GitHub
         </button>
-        <button className="flex items-center border p-4 rounded-md gap-4 hover:bg-slate-100/25 transition">
+        <button className="singup-btns">
           <span>
             <Image src={'/google-logo.svg'} width={30} height={30} alt={'Google Logo'}/>
           </span>

24c23b4071de2720d75e86d1b1a5a7499abe312e
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-07 14:20:38 +0530

    added posts in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import CategoriesList from "@/components/CategoriesList"
+import Post from "@/components/Post";
+import CategoriesList from "@/components/CategoriesList";
+import { posts } from "@/data";
 
 export default function Home() {
   return (
     <>
-      <CategoriesList/>
-      <div>
-        Post
-      </div>
+      <CategoriesList />
+      {posts && posts?.length > 0 ? (
+        posts?.map((post) => (<Post key={post?.id} postData={post}/>))
+      ) : (
+        <div>No posts to display!</div>
+      )}
     </>
   );
 }

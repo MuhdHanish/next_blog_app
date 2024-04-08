0942789d52677c51e1e53f6ce264dfd1093e006b
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-08 08:39:10 +0530

    created api/auth/[...nextauth]/ route.ts file

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import nextAuth, { AuthOptions } from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+const authOptions: AuthOptions = {
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string,
+};
+
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST };

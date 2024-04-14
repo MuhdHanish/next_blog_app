2a8bb7ad375c207915df035ae59a4c30a31f8452
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 10:05:26 +0530

    added route protection for sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
 import SignInButtons from "@/components/SignInButtons";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect(`/dashboard`);
+
   return <SignInButtons />;
 }

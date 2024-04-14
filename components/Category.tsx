9ba01bed7acbfecdbdb5c8164406ffe75b0a54fb
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 20:16:28 +0530

    used tiltle in category component

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function Category({ category }: { category: string }) {
+export default function Category({ title }: { title: string }) {
   return (
     <Link
       className="px-4 py-1 rounded-md text-sm bg-slate-800 text-white cursor-pointer"
-      href={`/categories/${category}`}
+      href={`/categories/${title}`}
     >
-      {category}
+      {title}
     </Link>
   );
 }

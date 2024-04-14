4a587e12b0de797c81205c4151aee21e5c6fff39
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 21:48:43 +0530

    added checking and sending the appropriate message to user

--- lib/useCases/posts/FindPostById/FindPostByIdController.ts
+++ lib/useCases/posts/FindPostById/FindPostByIdController.ts
@@ -9,8 +9,9 @@ export class FindPostByIdController {
   async handle(req: Request, res: Response, params: { id: string }) {
     try {
       const { id } = params;
-      if(!id) return this.responseHandler.customHandler("Post id is required", null, 400);
+      if (!id) return this.responseHandler.customHandler("Post id is required", null, 400);
       const post = await this.useCase.execute(id);
+      if (!post) return this.responseHandler.notFoundHandler("No post found for the provided id");
       return this.responseHandler.successHandler(post);
     } catch (error) {
       return this.responseHandler.serverErrorHandler(error);

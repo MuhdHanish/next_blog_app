5d66f14a7e43fe8d4922daf3bfc9713c7db432a5
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 09:44:44 +0530

    added image origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com"],
+    domains: ["images.pexels.com", "img.freepik.com", "cdn.create.vista.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   }
 };
 

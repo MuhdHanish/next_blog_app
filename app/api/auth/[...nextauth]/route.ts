08ef01f65712c5e08bda72f596e73e8cee8af27f
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 08:10:00 +0530

    added sign in  page in next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -13,6 +13,9 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  pages: {
+    signIn: "/sign-in",
+  },
   secret: process.env.NEXTAUTH_SECRET as string,
 };
 

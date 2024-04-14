3fd62a6a1ba7a95d3a52233805071740df7901aa
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 21:08:40 +0530

    used get handler in posts api

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,3 +1,5 @@
+import { findPostsController } from "@/lib/useCases/posts/FindPosts";
 import { createPostController } from "@/lib/useCases/posts/CreatePost";
 
+export const GET = async (req: Request, res: Response) => findPostsController.handle(req, res);
 export const POST = async (req: Request, res: Response) => createPostController.handle(req, res);

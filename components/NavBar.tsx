1e63f1b067d9d2e6eb266022ea262151b11e678d
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 09:45:04 +0530

    displayed user profile instead of sign out button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useSession, signOut } from "next-auth/react";
 
 export default function NavBar() {
-  const { status } = useSession();
+  const { status, data: session } = useSession();
   return (
     <nav className="flex justify-between pb-4  mb-4 border-b">
       <div>
@@ -18,9 +19,40 @@ export default function NavBar() {
       </div>
       <div className="flex items-center">
         {status === "authenticated" ? (
-          <button onClick={() => signOut()} className="btn">
-            Sign Out
-          </button>
+          // <button onClick={() => signOut()} className="btn">
+          //   Sign Out
+          // </button>
+          <>
+            <div className="flex gap-2 items-center">
+              <Link className="flex gap-1.5 items-center mr-6" href={`/create-post`}>
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Create Post</span>
+              </Link>
+              <Image
+                src={
+                  session?.user?.image ||
+                  "https://cdn.create.vista.com/api/media/small/356209164/stock-vector-user-avatar-illustration-anonymous-sign"
+                }
+                alt="Profile Image"
+                className="rounded-sm"
+                width={36}
+                height={36}
+              />
+            </div>
+          </>
         ) : (
           <Link className="btn" href={`/sign-in`}>
             Sign In

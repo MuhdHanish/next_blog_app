d415b05d6acacb9a8f291e16c0ea056268bf5288
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-14 21:04:37 +0530

    changed full create post  usecase to usecase

--- lib/useCases/posts/CreatePost/CreatePostUseCase.ts
+++ lib/useCases/posts/CreatePost/CreatePostUseCase.ts
@@ -1,11 +1,9 @@
 import { IPostsRepository } from "@/lib/respositories/posts/IPostsRepository";
 
 export class CreatePostUseCase {
-
-  constructor(private readonly postsRepository: IPostsRepository) { };
-  
+  constructor(private readonly repository: IPostsRepository) { };
   async execute(postData: any) {
-    const post = await this.postsRepository.createPost(postData);
+    const post = await this.repository.createPost(postData);
     return post;
   }
 }

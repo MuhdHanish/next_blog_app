eccdd23a161411e37f4de727e0c6afccc0ff6efe
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-15 09:11:59 +0530

    removed no content reponse handler from next reponse handler

--- lib/providers/responseHandler/implementations/NextResponseHanlder.ts
+++ lib/providers/responseHandler/implementations/NextResponseHanlder.ts
@@ -25,15 +25,6 @@ export class NextResponseHandler implements IResponseHandler {
     );
   }
 
-  noContentHandler(message: string) {
-    return NextResponse.json(
-      { message },
-      {
-        status: 204,
-      }
-    );
-  }
-
   serverErrorHandler(error: any) {
     console.error("Internal server error:", error);
     const errorMessage =

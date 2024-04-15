fbf28155fa08974c73802a739c1d5bea1ea194a3
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-15 09:11:49 +0530

    removed no content reponse handler

--- lib/providers/responseHandler/IResponseHandler.ts
+++ lib/providers/responseHandler/IResponseHandler.ts
@@ -2,7 +2,6 @@ export interface IResponseHandler {
   successHandler: (data: any) => void;
   createHandler: (data: any) => void;
   notFoundHandler: (message: string) => void;
-  noContentHandler: (message: string) => void;
   serverErrorHandler: (error: any) => void;
   customHandler: (message: string | string[], data: any , status: number) => void;
 }

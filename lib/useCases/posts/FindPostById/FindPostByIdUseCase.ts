f816c36800c61d4131b3266262f01f71e4d1b015
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-15 08:39:47 +0530

    created find post by id usecase

--- lib/useCases/posts/FindPostById/FindPostByIdUseCase.ts
+++ lib/useCases/posts/FindPostById/FindPostByIdUseCase.ts
@@ -3,8 +3,8 @@ import { IPostsRepository } from "@/lib/respositories/posts/IPostsRepository";
 export class FindPostByIdUseCase {
   constructor(private readonly repository: IPostsRepository) { };
   async execute(id: string) {
-    const posts = await this.repository.findPostById(id);
-    return posts;
+    const post = await this.repository.findPostById(id);
+    return post;
   }
 
 }

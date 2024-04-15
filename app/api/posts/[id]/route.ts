2ec0fc27f301693c06364a1fb2a8b80d02d3bba0
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-15 08:40:54 +0530

    added PUT handler in posts api

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,7 +1,18 @@
 import { findPostByIdController } from "@/lib/useCases/posts/FindPostById";
+import { findPostByIdAndUpdateController } from "@/lib/useCases/posts/FindPostByIdAndUpdate";
+
+type TRequestParams = {
+    id: string 
+}
 
 export const GET = async (
   req: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: TRequestParams },
   res: Response
 ) => findPostByIdController.handle(req, res, params);
+
+export const PUT = async (
+  req: Request,
+  { params }: { params : TRequestParams },
+  res: Response
+) => findPostByIdAndUpdateController.handle(req, res, params);

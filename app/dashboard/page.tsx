0518130b1cd5364c774cf1e2a74e7108f1625ecd
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 21:12:18 +0530

    changed the function name and handled error more manner way and also added force-dynamic to avoid caching and dynamic rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,14 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { TPost } from "@/types";
 import { redirect } from "next/navigation";
 import PostsList from "@/components/PostsList";
 import { getServerSession } from "next-auth/next";
+import NoDataFound from "@/components/NoDataFound";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
-async function getUserPostsWithEmail(email: string): Promise<TPost[] | undefined> {
+async function findUserWithPostsByEmail(email: string): Promise<TPost[] | undefined> {
   try {
     let response = await fetch(`${process.env.NEXTAUTH_URL}/api/users/${email}`, { cache: "no-store" });
     if (response.ok) {
@@ -13,31 +16,34 @@ async function getUserPostsWithEmail(email: string): Promise<TPost[] | undefined
       const { data } = responseData;
       const { posts } = data;
       if (!posts || !Array.isArray(posts)) {
-        throw new Error("Invalid data format");
+          throw new Error(`Invalid data format\nReceived: ${JSON.stringify(posts, null, 2)}`);
       }
       return posts;
+    } else {
+      throw new Error(`Request failed, HTTP Status Code : ${response.status}`);
     }
   } catch (error) {
     console.error(error);
   }
 }
 
+
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
   if (!session) return redirect(`/sign-in`);
   let posts:TPost[] = [];
   const email = session?.user?.email;
-  if(email) posts = (await getUserPostsWithEmail(email) || []);
+  if(email) posts = (await findUserWithPostsByEmail(email)) || [];
   return (
     <div>
       <h1>My Posts</h1>
       {posts && posts?.length > 0 ? (
         <PostsList posts={posts} />
       ) : (
-          <div className="py-6">
-            No posts created yet! {" "}
-            <Link className="underline" href={`/create-post`}>Create Post</Link>
-          </div>
+        <NoDataFound>
+          No posts created yet! {" "}
+          <Link className="underline" href={`/create-post`}>Create Post</Link>
+        </NoDataFound>
       )}
     </div>
   );

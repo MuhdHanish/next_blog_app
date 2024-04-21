7f15eed2af1bb5187b63eda2c42ac9034a2c8caa
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 16:10:29 +0530

    added delete button functionality

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,3 +1,23 @@
-export default function DeleteButton() {
-  return <button className="text-red-600 editable-btns">Delete</button>;
+"use client";
+
+export default function DeleteButton({ id }: { id: string }) {
+  const handleDelete = async () => {
+    const confirmed = window.confirm("Are you sure you want to delete this post ?")
+    if (confirmed) {
+      try {
+        const response = await fetch(`/api/posts/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        if (!response.ok) {
+          throw new Error(`Error on deleting post`);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+  return <button onClick={handleDelete} className="text-red-600 editable-btns">Delete</button>;
 }

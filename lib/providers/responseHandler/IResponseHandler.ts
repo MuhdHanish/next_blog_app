99b518844792260cb65286f2cade17ac1caf62c3
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 15:23:25 +0530

    added un authorized  and access forbidden function interface

--- lib/providers/responseHandler/IResponseHandler.ts
+++ lib/providers/responseHandler/IResponseHandler.ts
@@ -2,6 +2,8 @@ export interface IResponseHandler {
   successHandler: (data: any) => void;
   createHandler: (data: any) => void;
   notFoundHandler: (message: string) => void;
+  unAuthenticatedHandler: () => void;
+  accessForbiddenHandler: () => void;
   serverErrorHandler: (error: any) => void;
   customHandler: (message: string | string[], data: any , status: number) => void;
 }

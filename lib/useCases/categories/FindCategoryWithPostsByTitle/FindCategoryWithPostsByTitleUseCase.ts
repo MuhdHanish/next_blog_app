7678c39288684b14564b019e2dd683703413c22e
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 11:24:48 +0530

    created the find  user with posts by email usecase

--- lib/useCases/categories/FindCategoryWithPostsByTitle/FindCategoryWithPostsByTitleUseCase.ts
+++ lib/useCases/categories/FindCategoryWithPostsByTitle/FindCategoryWithPostsByTitleUseCase.ts
@@ -1,11 +1,10 @@
 import { ICategoriesRepository } from "@/lib/respositories/categories/ICategoriesRepository";
 
-
 export class FindCategoryWithPostsByTitleUseCase {
   constructor(private readonly repository: ICategoriesRepository) { };
   async execute(title: string) {
-    const categories = await this.repository.findCategoryWithPostsByTitle(title);
-    return categories;
+    const category = await this.repository.findCategoryWithPostsByTitle(title);
+    return category;
   }
 
 }

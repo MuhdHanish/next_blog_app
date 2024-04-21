270e6290237b49985a1e407e5deb75f4be4927e1
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 21:11:14 +0530

    changed /categories/[title] to /category/[title]

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,7 +4,7 @@ export default function Category({ title }: { title: string }) {
   return (
     <Link
       className="px-4 py-1 rounded-md text-sm bg-slate-800 text-white cursor-pointer"
-      href={`/categories/${title}`}
+      href={`/category/${title}`}
     >
       {title}
     </Link>

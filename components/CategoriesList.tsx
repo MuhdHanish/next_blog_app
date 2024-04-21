c29034844a76dcfdae79a6eacd37aec68888ac6d
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 15:24:52 +0530

    added the categories GET api integration

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -1,11 +1,28 @@
 import Category from "./Category";
-import { categoriesList } from "@/data";
+import { TCategory } from "@/types";
 
-export default function CategoriesList() {
+async function getCategories(): Promise<TCategory[] | undefined> {
+  try {
+    let response = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`);
+    if (response.ok) {
+      const responseData = await response.json();
+      const { data } = responseData;
+      if (!data || !Array.isArray(data)) {
+        throw new Error("Invalid data format");
+      }
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function CategoriesList() {
+  const categories = (await getCategories()) || [];
   return (
     <div className="flex gap-2 flex-wrap">
-      {categoriesList &&
-        categoriesList?.map((category) => (
+      {categories &&
+        categories?.map((category) => (
           <Category key={category?.id} title={category?.title} />
         ))}
     </div>

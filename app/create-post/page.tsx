1ec4278436610bcb7732bb01c5d92fd32a74d758
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-21 21:12:58 +0530

    make appro0priate changes in create post page

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
-import CreatePostForm from "@/components/CreatePostForm";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import PostForm from "@/components/PostForm";
 
 export default async function CreatePost() {
   const session = await getServerSession(authOptions);
   if (!session) return redirect(`/sign-in`);
 
-  return <CreatePostForm/>;
+  return <PostForm/>;
 }

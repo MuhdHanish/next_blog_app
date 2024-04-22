a9c8e1ce8dd8d0b2b07bde5ff77f45a77023de38
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-22 08:47:43 +0530

    added cloudinary base url in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com", "img.freepik.com", "cdn.create.vista.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "cdn.create.vista.com",
+      "images.pexels.com",
+      "img.freepik.com",
+    ],
   }
 };
 

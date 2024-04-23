a017308d77ef7df79f4dcf54440e80d45571f15d
MuhdHanish <muhammedhanish11@gmail.com>
2024-04-23 07:37:21 +0530

    added delete the post image also when user try to delete a post

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-export default function DeleteButton({ id }: { id: string }) {
+import { TPost } from "@/types";
+
+export default function DeleteButton({ post }: { post: TPost }) {
   const handleDelete = async () => {
     const confirmed = window.confirm("Are you sure you want to delete this post ?")
     if (confirmed) {
       try {
-        const response = await fetch(`/api/posts/${id}`, {
+        const postResponse = await fetch(`/api/posts/${post?.id}`, {
           method: "DELETE",
           headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+          },
+        });
+        if (postResponse.ok) {
+          if (post?.publicId) {
+            const imageResponse = await fetch(`/api/images/${post?.publicId}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            if (!imageResponse.ok) {
+              throw new Error(`Error on deleting post image`);
+            }
           }
-        })
-        if (!response.ok) {
+        } else {
           throw new Error(`Error on deleting post`);
         }
       } catch (error) {

743d2c2ad0c874c85df6070785af996e833e0abb
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-04 22:23:10 +0530

    used fetch request function from utils in delete button and used toast

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,37 +1,24 @@
 "use client";
-
+import { toast } from "sonner";
 import { TPost } from "@/types";
+import { format, request } from "@/utils";
+import { useRouter } from "next/navigation";
 
 export default function DeleteButton({ post }: { post: TPost }) {
+  const router = useRouter();
   const handleDelete = async () => {
     const confirmed = window.confirm("Are you sure you want to delete this post ?")
     if (confirmed) {
       try {
-        const postResponse = await fetch(`/api/posts/${post?.id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (postResponse.ok) {
-          if (post?.publicId) {
-            const imageResponse = await fetch(`/api/images/${post?.publicId}`, {
-              method: "DELETE",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-            if (!imageResponse.ok) {
-              throw new Error(`Error on deleting post image`);
-            }
-          }
-        } else {
-          throw new Error(`Error on deleting post`);
-        }
+        await request({ method: "DELETE", url: `/posts/${post?.id}`, isClient: true })
+        const description = format();
+        toast("Post has been deleted", { description });
       } catch (error) {
-        console.error(error);
+        toast.error("Post has not been deleted");
       }
+      post?.publicId && await request({ method: "DELETE", url: `/images/${post?.publicId}`, isClient: true });
+      router.refresh();
     }
   }
-  return <button onClick={handleDelete} className="text-red-600 editable-btns">Delete</button>;
+  return <button onClick={handleDelete} className="text-red-600 editable-btns">DELETE</button>;
 }

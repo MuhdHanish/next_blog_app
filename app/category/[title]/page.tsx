5f5335f9a5f3a6e5f5e9602d3daa9df864064f3b
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-04 22:22:12 +0530

    used fetch request function from utils in category page

--- app/category/[title]/page.tsx
+++ app/category/[title]/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = "force-dynamic";
-
-import { TPost } from "@/types";
+import { request } from "@/utils";
+import { TPost, TRequestProps } from "@/types";
 import PostsList from "@/components/PostsList";
 import NoDataFound from "@/components/NoDataFound";
 
@@ -8,28 +7,19 @@ type TRequestParams = {
   title: string;
 };
 
-async function findCategoryWithPostsByTitle(title: string): Promise<TPost[] | undefined> {
-  try {
-    let response = await fetch(`${process.env.NEXTAUTH_URL}/api/categories/${title}`, { cache: "no-store" });
-    if (response.ok) {
-      const responseData = await response.json();
-      const { data } = responseData;
-      const { posts } = data;
-      if (!posts || !Array.isArray(posts)) {
-          throw new Error(`Invalid data format\nReceived: ${JSON.stringify(posts, null, 2)}`);
-      }
-      return posts;
-    } else {
-      throw new Error(`Request failed, HTTP Status Code : ${response.status}`);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-export default async function CategoryPosts({ params }: { params: TRequestParams }) {
+export default async function CategoryPosts({
+  params,
+}: {
+  params: TRequestParams;
+}) {
   const { title } = params;
-  const posts = (await findCategoryWithPostsByTitle(title)) || undefined;
+  const options: TRequestProps = {
+    url: `/categories/${title}`,
+    keyName: "posts",
+    returnType: "array",
+    method: "GET",
+  };
+  const posts = (await request(options)) as TPost[] | undefined;
   return (
     <>
       <h1>

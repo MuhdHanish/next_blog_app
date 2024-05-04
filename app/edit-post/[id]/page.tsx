810b3111d8fd8f08fa7af15d5c7afaadb3e9751e
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-04 22:22:45 +0530

    used fetch request function from utils in edit post

--- app/edit-post/[id]/page.tsx
+++ app/edit-post/[id]/page.tsx
@@ -1,8 +1,9 @@
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
-import { TPost } from "@/types";
+import { request } from "@/utils";
 import { redirect } from "next/navigation";
 import PostForm from "@/components/PostForm";
+import { TRequestProps, TPost } from "@/types";
 import { getServerSession } from "next-auth/next";
 import NoDataFound from "@/components/NoDataFound";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
@@ -11,29 +12,16 @@ type TRequestParams = {
   id: string;
 };
 
-async function findPostById(id: string): Promise<TPost | undefined> {
-  try {
-    let response = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/${id}`, { cache: "no-store" });
-    if (response.ok) {
-      const responseData = await response.json();
-      const { data } = responseData;
-      if (!data) {
-        throw new Error(`Invalid data format\nReceived: ${JSON.stringify(data, null, 2)}`);
-      }
-      return data;
-    } else {
-      throw new Error(`Request failed, HTTP Status Code : ${response.status}`);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default async function EditPost({ params }: { params: TRequestParams }) {
   const session = await getServerSession(authOptions);
   if (!session) return redirect(`/sign-in`);
   const { id } = params;
-  const post = (await findPostById(id)) || undefined;
+  const options: TRequestProps = {
+    url: `/posts/${id}`,
+    returnType: "object",
+    method: "GET",
+  };
+  const post = (await request(options)) as TPost | undefined;
   return (
     <>
       {post ? (

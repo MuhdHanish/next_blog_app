434021e63114616a02c1e541d3fa98cd211537e7
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-04 22:21:54 +0530

    used fetch request function from utils in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,16 @@
-export const revalidate = 0;
-
-import { TPost } from "@/types";
+import { request } from "@/utils";
+import { TRequestProps, TPost } from "@/types";
 import PostsList from "@/components/PostsList";
 import NoDataFound from "@/components/NoDataFound";
 import CategoriesList from "@/components/CategoriesList";
 
-async function findPosts(): Promise<TPost[] | undefined> {
-  try {
-    let response = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, { cache: "no-store" });
-    if (response.ok) {
-      const responseData = await response.json();
-      const { data } = responseData;
-      if (!data || !Array.isArray(data)) {
-        throw new Error(`Invalid data format\nReceived: ${JSON.stringify(data, null, 2)}`);
-      }
-      return data;
-    } else {
-      throw new Error(`Request failed, HTTP Status Code : ${response.status}`);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default async function Home() {
-  const posts = (await findPosts()) || [];
+  const options: TRequestProps = {
+    url: `/posts`,
+    returnType: "array",
+    method: "GET",
+  };
+  const posts = (await request(options)) as TPost[] | undefined;
   return (
     <>
       <CategoriesList />

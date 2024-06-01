e93da0d57bbe7c506ff35e8c6b9b8bea57b34223
Muhammed Hanish <122072991+MuhdHanish@users.noreply.github.com>
2024-06-01 14:58:07 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <footer className="py-3 text-sm">Next Blog | Explore more on my blog!</footer>;
+  return <footer className="py-3 text-sm">Next Blog | Muhd Hanish</footer>;
 }
